a3d0099878973ff3a3a31b3d7a1a1ac9f9ea26da
lawsy <law.jonny@googlemail.com>
2016-08-24 08:33:41 +0100

    Shell script to build website

--- Website/knitWebsite.sh
+++ Website/knitWebsite.sh
@@ -2,3 +2,5 @@ for f in *.Rmd; do
     name=`echo $f | cut -f1 -d.`
     Rscript -e "require(knitr); require(markdown); knit('${name}.Rmd', '${name}.md'); markdownToHTML('${name}.md', '${name}.html', options=c('use_xhtml', 'base64_images'))";
 done
+
+rm *.md
