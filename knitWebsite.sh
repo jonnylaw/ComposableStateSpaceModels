36495b673c588b5d35eba5344337a64e0c691d79
lawsy <law.jonny@googlemail.com>
2016-09-05 15:34:29 +0100

    Work on documentation

--- knitWebsite.sh
+++
@@ -1,6 +0,0 @@
-for f in *.Rmd; do
-    name=`echo $f | cut -f1 -d.`
-    Rscript -e "require(knitr); require(markdown); knit('${name}.Rmd', '${name}.md'); markdownToHTML('${name}.md', '${name}.html', options=c('use_xhtml', 'base64_images'))";
-done
-
-rm *.md
